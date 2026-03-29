ece313612eacccad233dbbd58a1338d15195a9de
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-03-29 09:41:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -6,9 +6,20 @@ class Solution {
         // int n = val.length;
         // return knapsackrec(W , val , wt , n);
         
-        return knapsackTab(W , val , wt);
+        // using knapsackTab
+        // return knapsackTab(W , val , wt);
+        
+        // using memozation //
+        int n = val.length;
+        int dp[][] = new int[n + 1][W  + 1];
+        for(int i = 0; i <= n; i++){
+            Arrays.fill(dp[i] , - 1);
+        }
+        return kanpsackmem(W , val , n , wt ,dp);
         
     }
+    
+    // recursion 
     // int knapsackrec(int W , int val[] , int wt[] , int n){
     //     if(n == 0 || W == 0){
     //         return 0;
@@ -23,19 +34,36 @@ class Solution {
     
     // using tabulation method
     
-    static int knapsackTab(int W , int val[] , int wt[] ){
-        int n = val.length;
-        int dp[][] = new int[n + 1][W + 1];
-        for(int i = 1; i <= n; i++){
-            for(int j  = 1; j <= W; j++){
-               int pick = 0;
-               if(wt[i - 1] <= j){
-                   pick = val[i - 1] + dp[i - 1] [j - wt[i - 1]];
-               }
-               int notpick = dp[i - 1][j];
-               dp[i][j] = Math.max(pick , notpick);
-            }
-        }
-            return dp[n][W];
-    }
+    // static int knapsackTab(int W , int val[] , int wt[] ){
+    //     int n = val.length;
+    //     int dp[][] = new int[n + 1][W + 1];
+    //     for(int i = 1; i <= n; i++){
+    //         for(int j  = 1; j <= W; j++){
+    //           int pick = 0;
+    //           if(wt[i - 1] <= j){
+    //               pick = val[i - 1] + dp[i - 1] [j - wt[i - 1]];
+    //           }
+    //           int notpick = dp[i - 1][j];
+    //           dp[i][j] = Math.max(pick , notpick);
+    //         }
+    //     }
+    //         return dp[n][W];
+    // }
+    
+    // using memoization 
+     int kanpsackmem(int W , int val[] ,int n , int wt[] , int dp[][]){
+         if(n == 0 || W == 0){
+             return 0;
+         }
+         if(dp[n][W] != -1){
+             return dp[n][W];
+         }
+         int pick = 0;
+         if(wt[n - 1] <= W){
+             pick = val[n - 1] + kanpsackmem(W - wt[n - 1] , val , n - 1 , wt , dp);
+         }
+         int notpick = kanpsackmem(W ,val , n - 1 , wt , dp);
+         dp[n][W] = Math.max(pick , notpick);
+         return dp[n][W];
+     }
 }
