eabdcef7955c160a20689cdaf90b22971c8d09f1
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-07 23:29:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -4,19 +4,19 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
         
+        int max = 0;
         if(n == 0 || m == 0){
             return 0;
         }
+        int dp[][]= new int[n + 1][m + 1];
         
-        int max = 0;
-        int dp[][] = new int[n + 1][m + 1];
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-                    dp[i][j] =  1 + dp[i - 1][j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                     max = Math.max(max , dp[i][j]);
-                }else {
-                   dp[i][j] = 0;
+                }else{
+                    dp[i][j] = 0;
                 }
             }
         }
