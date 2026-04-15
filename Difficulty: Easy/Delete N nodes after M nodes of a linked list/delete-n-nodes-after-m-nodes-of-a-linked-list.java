19029bda8a1e94353ee5973825de4f70aa6c81bf
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-15 23:52:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete N nodes after M nodes of a linked list/delete-n-nodes-after-m-nodes-of-a-linked-list.java
@@ -0,0 +1,39 @@
+/*
+delete n nodes after m nodes
+The input list will have at least one element
+Node is defined as
+  class Node
+  {
+      int data;
+      Node next;
+      Node(int data)
+      {
+          this.data = data;
+          this.next = null;
+      }
+  }
+*/
+
+class Solution {
+    static void linkdelete(Node head, int n, int m) {
+        // your code here
+            Node curr = head;
+            while(curr != null){
+                for(int i = 1; i < m && curr != null; i++){
+                    curr = curr.next;
+                }
+                if(curr == null){
+                    return;
+                }
+                Node temp = curr.next;
+                for(int j = 1; j <= n && temp != null; j++){
+                    temp = temp.next;
+                }
+                curr.next = temp;
+                curr  = temp;
+            }
+    }  
+}
+
+
+  
