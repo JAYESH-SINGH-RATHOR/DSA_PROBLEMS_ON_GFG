43238e7118bdd65827eec2ab72a053a4d513f38c
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-20 12:16:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.java
+++ Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.java
@@ -14,16 +14,19 @@ class Solution {
     int diameter = 0;
     public int diameter(Node root) {
         // code here
-        rec(root);
-        return diameter;
+        height(root);
+        return diameter;   
     }
-    int rec(Node root){
+     int height(Node root){
         if(root == null){
             return 0;
         }
-        int lh = rec(root.left);
-        int rh = rec(root.right);
+        int lh = height(root.left);
+        int rh = height(root.right);
         diameter = Math.max(diameter , lh + rh);
-        return Math.max(lh ,rh) + 1;
+        return Math.max(lh , rh) + 1;
     }
 }
+
+
+   
