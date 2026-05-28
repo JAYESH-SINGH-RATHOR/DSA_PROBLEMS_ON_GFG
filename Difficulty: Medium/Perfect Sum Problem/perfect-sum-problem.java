b9bf0120c4477b75dd9ea4b5128ac60500148300
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-05-28 07:14:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int perfectSum(int[] nums, int target) {
+
+        int n = nums.length;
+
+        int dp[][] = new int[n + 1][target + 1];
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(nums[i - 1] <= j) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+}
