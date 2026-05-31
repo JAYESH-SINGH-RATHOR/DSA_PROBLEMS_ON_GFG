6cda4e7b9a804b0814ac97c9c43119f9b0ec0349
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-05-31 14:37:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -2,68 +2,48 @@ class Solution {
     public int knapsack(int W, int val[], int wt[]) {
         // code here
         
-        // using only recursion 
-        // int n = val.length;
-        // return knapsackrec(W , val , wt , n);
-        
-        // using knapsackTab
-        // return knapsackTab(W , val , wt);
-        
-        // using memozation //
+        // using memoizations
         int n = val.length;
-        int dp[][] = new int[n + 1][W  + 1];
+        int dp[][] = new int[val.length + 1][W + 1];
         for(int i = 0; i <= n; i++){
-            Arrays.fill(dp[i] , - 1);
+            for(int j = 0; j <= W; j++){
+                dp[i][j] = -1;
+            }
         }
-        return kanpsackmem(W , val , n , wt ,dp);
+        return solvemem(W , val , wt , dp , n);
+        // using recursion
         
+        // int n = val.length;
+        // return solve(W , val , wt , n);
     }
-    
-    // recursion 
-    // int knapsackrec(int W , int val[] , int wt[] , int n){
+    // int solve(int W , int val[] , int wt[] , int n){
     //     if(n == 0 || W == 0){
     //         return 0;
     //     }
-    //     int pick = 0;
     //     if(wt[n - 1] <= W){
-    //     pick = val[n - 1] + knapsackrec(W - wt[n - 1] , val , wt , n - 1 );
+    //       int include = val[n - 1] + solve(W - wt[n - 1], val, wt, n - 1);
+    //       int exclude = solve(W, val, wt, n - 1);
+    //         return Math.max(include, exclude);
+
     //     }
-    //     int notpick = knapsackrec(W , val , wt , n - 1);
-    //     return Math.max(pick , notpick);
+    //     return solve(W, val, wt, n - 1);
     // }
     
-    // using tabulation method
-    
-    // static int knapsackTab(int W , int val[] , int wt[] ){
-    //     int n = val.length;
-    //     int dp[][] = new int[n + 1][W + 1];
-    //     for(int i = 1; i <= n; i++){
-    //         for(int j  = 1; j <= W; j++){
-    //           int pick = 0;
-    //           if(wt[i - 1] <= j){
-    //               pick = val[i - 1] + dp[i - 1] [j - wt[i - 1]];
-    //           }
-    //           int notpick = dp[i - 1][j];
-    //           dp[i][j] = Math.max(pick , notpick);
-    //         }
-    //     }
-    //         return dp[n][W];
-    // }
-    
-    // using memoization 
-     int kanpsackmem(int W , int val[] ,int n , int wt[] , int dp[][]){
-         if(n == 0 || W == 0){
-             return 0;
-         }
-         if(dp[n][W] != -1){
-             return dp[n][W];
-         }
-         int pick = 0;
-         if(wt[n - 1] <= W){
-             pick = val[n - 1] + kanpsackmem(W - wt[n - 1] , val , n - 1 , wt , dp);
-         }
-         int notpick = kanpsackmem(W ,val , n - 1 , wt , dp);
-         dp[n][W] = Math.max(pick , notpick);
-         return dp[n][W];
-     }
+    static int solvemem(int W , int val[] , int wt[] , int dp[][] , int n){
+        if(W == 0 || n == 0){
+            return 0;
+        }
+        if(dp[n][W] != -1){
+            return dp[n][W];
+        }
+        
+        if(wt[n - 1] <= W){
+            int include = val[n - 1] + solvemem(W - wt[n - 1] , val , wt , dp , n - 1);
+            int exclude = solvemem(W , val , wt , dp , n - 1 );
+             dp[n][W] = Math.max(include , exclude);
+            return dp[n][W];
+        }
+        dp[n][W] = solvemem(W , val , wt , dp , n - 1);
+        return dp[n][W];
+    }
 }
