5bccdb53e8fbcbb2e70c4da9e13ababee29a0c39
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-11 16:43:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -1,21 +1,30 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
 class Solution {
     public boolean isPalindrome(Node head) {
         // code here
-        Stack<Integer> st = new Stack<>();
-        Node temp = head;
-        while(temp != null){
-            st.push(temp.data);
-            temp = temp.next;
-        }
-        
-        temp = head;
-        while(temp != null){
-            if(temp.data != st.pop()){
-                return false;
-            }
-            temp = temp.next;
-        }
-        return true;
+    Stack<Integer> st = new Stack<>();
+    Node temp = head;
+    while(temp != null){
+        st.push(temp.data);
+        temp = temp.next;
     }
-    
+    temp = head;
+    while(!st.isEmpty()){
+        if(temp.data != st.pop()){
+            return false;
+        }
+            temp = temp.next;
+    }
+    return true;
+    }
 }
