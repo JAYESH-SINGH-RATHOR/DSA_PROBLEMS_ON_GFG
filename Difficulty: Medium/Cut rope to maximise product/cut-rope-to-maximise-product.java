de98c728e0d0030bf4946d040d78b86f87e2c5a0
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-17 15:00:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Cut rope to maximise product/cut-rope-to-maximise-product.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int n) {
+        // code here
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        
+        int max = 1;
+        while(n > 4){
+            max *= 3;
+            n -= 3;
+        }
+        return max * n;
+    }
+}
