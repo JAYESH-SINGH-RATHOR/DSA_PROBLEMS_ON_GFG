8a5a6ca7883d81b10117721231cc35de89000e75
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-22 22:45:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Queue Reversal/queue-reversal.java
+++ Difficulty: Easy/Queue Reversal/queue-reversal.java
@@ -1,12 +1,20 @@
 class Solution {
     public void reverseQueue(Queue<Integer> q) {
         // code here
-       Stack<Integer> s = new Stack<>();
-       while(!q.isEmpty()){
-           s.push(q.remove());
-       }
-       while(!s.isEmpty()){
-           q.add(s.pop());
-       }
+    //   Stack<Integer> s = new Stack<>();
+    //   while(!q.isEmpty()){
+    //       s.push(q.remove());
+    //   }
+    //   while(!s.isEmpty()){
+    //       q.add(s.pop());
+    //   }
+    
+    // secound method 
+     if (q.isEmpty()) {
+            return;
+        }
+        int front = q.remove();
+        reverseQueue(q);
+        q.add(front);
     }
 }
