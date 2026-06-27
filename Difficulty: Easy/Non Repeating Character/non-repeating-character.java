2f62d11858120ee7126407949c149d88e357fbe2
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-27 09:42:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,19 +1,19 @@
 class Solution {
     public char nonRepeatingChar(String s) {
-        // code here
-        int freq[] = new int[26];
-        Queue<Character> q = new LinkedList<>();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            freq[ch - 'a']++;
-            q.add(ch);
-            while(!q.isEmpty() && freq[q.peek() - 'a'] > 1){
-            q.remove();
-        }
-        }
-        if(q.isEmpty()){
-            return '$';
-        }
-        return q.peek();
+       int n = s.length();
+       HashMap<Character , Integer> map = new HashMap<>();
+       for(int i = 0; i < n; i++){
+           char e = s.charAt(i);
+          map.put(e, map.getOrDefault(e, 0) + 1);
+       }
+    //   System.out.print(map);
+       for(int i = 0; i < n; i++){
+           char ch = s.charAt(i);
+           if(map.get(ch) == 1){
+               return ch;
+           }
+            //   System.out.print("ch wil be " + ch);
+       }
+       return '$';
     }
 }
