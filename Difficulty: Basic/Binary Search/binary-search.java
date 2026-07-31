668d2012a5e8d3d3a15fb9a0e75607298e1110a2
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-31 15:42:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Binary Search/binary-search.java
+++ Difficulty: Basic/Binary Search/binary-search.java
@@ -1,17 +1,19 @@
 class Solution {
-    static boolean searchInSorted(int arr[], int k) {
-        // Your code here
+    public boolean binarySearch(int[] arr, int k) {
+        // code here
         int left = 0;
         int right = arr.length - 1;
         while(left <= right){
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) /2;
             if(arr[mid] == k){
                 return true;
-            }else if(arr[mid] < k){
-                left = mid + 1;
+            }
+            if(arr[mid] < k){
+                left= mid + 1;
             }else{
                 right = mid - 1;
             }
+            
         }
         return false;
     }
