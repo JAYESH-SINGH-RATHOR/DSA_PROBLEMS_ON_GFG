a4aa514aa08841f192b184025d840e3a5881ac62
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-02 00:35:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Meeting Rooms/meeting-rooms.java
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.java
@@ -6,8 +6,8 @@ class Solution {
         for(int i = 0; i < arr.length - 1; i++){
             if(arr[i][1] > arr[i + 1][0]){
                 return false;
+            }
         }
-    }
         return true;
     }
 }
