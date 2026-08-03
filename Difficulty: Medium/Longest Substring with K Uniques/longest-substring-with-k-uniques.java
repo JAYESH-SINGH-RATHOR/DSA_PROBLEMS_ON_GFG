149950cc26102f5c88d05075e9650d487cbb5002
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-03 12:16:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,32 +1,28 @@
 class Solution {
     public int longestKSubstr(String s, int k) {
         // code here
-        int left = 0;
-        int max = -1;
-        int n = s.length();
-        HashMap<Character , Integer > map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            char ch = s.charAt(i);
-            if(!map.containsKey(ch)){
-                map.put(ch , 0);
-            }
-            int freq = map.get(ch);
-            map.put(ch , freq + 1);
-            while(map.size() > k){
-                char leftch = s.charAt(left);
-                if(!map.containsKey(leftch)){
-                    map.put(ch , 0);
-                }
-                map.put(leftch , map.get(leftch) - 1);
-                if(map.get(leftch) == 0){
-                    map.remove(leftch);
-                }
-                left++;
-            }
-            if(map.size() == k){
-                max = Math.max(max , i - left + 1);
-            }
-        }
-        return max;
+       int left = 0;
+       int max = -1;
+       HashMap<Character , Integer> map = new HashMap<>();
+       for(int i = 0; i < s.length(); i++){
+           char ch = s.charAt(i);
+           if(!map.containsKey(ch)){
+               map.put(ch , 0);
+           }
+           int freq = map.get(ch);
+           map.put(ch , freq + 1);
+           while(map.size() > k){
+               char leftch = s.charAt(left);
+               map.put(leftch , map.get(leftch) - 1);
+               if(map.get(leftch) == 0){
+                   map.remove(leftch);
+               }
+               left++;
+           }
+           if(map.size() == k){
+               max = Math.max(max , i - left + 1);
+           }
+       }
+       return max;
     }
 }
