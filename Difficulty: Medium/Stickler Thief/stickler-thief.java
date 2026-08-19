28dfabd9ff06e8e8456a2c2b57abeb9d8502766d
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-19 06:09:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -1,23 +1,13 @@
 class Solution {
     public int findMaxSum(int arr[]) {
         // code here
-        
-        // using tabulation method;
-        return findmaxtab(arr);
-        // using memozation method //
-        
+    int n = arr.length;
+    int dp[] = new int[n + 1];
+    dp[0] = arr[0];
+    dp[1] = Math.max(arr[0] , arr[1]);
+    for(int i = 2; i < n; i++){
+        dp[i] = Math.max(dp[i - 1] , arr[i] + dp[i - 2]);
     }
-    int findmaxtab(int arr[]){
-        int n = arr.length;
-        int dp[] = new int[n + 1];
-        if(n == 1){
-            return arr[0];
-        }
-        dp[0] =arr[0];
-        dp[1] =Math.max(arr[0] , arr[1]);
-        for(int i = 2; i < n; i++){
-            dp[i] =  Math.max( arr[i] + dp[i - 2] , dp[ i - 1]);
-        }
-        return dp[n - 1];
+    return dp[n - 1];
     }
 }
