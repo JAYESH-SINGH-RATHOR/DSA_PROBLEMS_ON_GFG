ac3bac81b001250adfd95027045aade64077d40f
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-07 16:08:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.java
+++ Difficulty: Easy/Identical Trees/identical-trees.java
@@ -15,12 +15,13 @@ class Solution {
         if(r1 == null && r2 == null){
             return true;
         }
-        if(r1 == null || r2 == null){
+        if(r1 == null || r2 == null ){
             return false;
         }
         if(r1.data != r2.data){
             return false;
         }
-        return isIdentical(r1.left , r2.left) && isIdentical(r1.right , r2.right);
+        return isIdentical(r1.left , r2.left)
+        && isIdentical(r1.right , r2.right);
     }
 }
