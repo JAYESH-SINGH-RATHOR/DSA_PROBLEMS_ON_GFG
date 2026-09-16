19ca22d305005371b5d0febc0a94a0326080a417
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-16 09:56:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,12 +1,59 @@
+    // using dfs 
+
+// class Solution {
+//     public boolean isCyclic(int V, int[][] edges) {
+//         // code here
+//         ArrayList<ArrayList<Integer>> adj  = new ArrayList<>();
+//         boolean visited[] = new boolean[V];
+//         boolean pathvisited[] = new boolean[V];
+//         for(int i = 0; i < V; i++){
+//             adj.add(new ArrayList<>());
+//         }
+//         for(int e[] : edges){
+//             int u = e[0];
+//             int v = e[1];
+//             adj.get(u).add(v);
+//         }
+//         for(int i = 0; i < V; i++){
+//             if(!visited[i]){
+//                 if(dfsUtil(adj , i , visited , pathvisited)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+//     boolean dfsUtil( ArrayList<ArrayList<Integer>> adj  , int curr ,
+//     boolean visited[] , boolean pathvisited[]){
+//         visited[curr] = true;
+//         pathvisited[curr] = true;
+//         for(int e : adj.get(curr)){
+//             if(pathvisited[e]){
+//                 return true;
+//             }
+//             if(!visited[e]){
+//                 if(dfsUtil(adj , e , visited , pathvisited)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         pathvisited[curr] = false;
+//         return false;
+//     }
+// }
+
+// using topological sort 
+
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
-        // code here
-        ArrayList<ArrayList<Integer>> adj  = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // Stack<Integer> st = new Stack<>();
         boolean visited[] = new boolean[V];
         boolean pathvisited[] = new boolean[V];
         for(int i = 0; i < V; i++){
             adj.add(new ArrayList<>());
         }
+        
         for(int e[] : edges){
             int u = e[0];
             int v = e[1];
@@ -14,15 +61,15 @@ class Solution {
         }
         for(int i = 0; i < V; i++){
             if(!visited[i]){
-                if(dfsUtil(adj , i , visited , pathvisited)){
+                if(toposort(adj , i , pathvisited, visited)){
                     return true;
                 }
             }
         }
         return false;
     }
-    boolean dfsUtil( ArrayList<ArrayList<Integer>> adj  , int curr ,
-    boolean visited[] , boolean pathvisited[]){
+    boolean toposort(ArrayList<ArrayList<Integer>> adj , int curr ,
+     boolean pathvisited[] , boolean visited[] ){
         visited[curr] = true;
         pathvisited[curr] = true;
         for(int e : adj.get(curr)){
@@ -30,7 +77,7 @@ class Solution {
                 return true;
             }
             if(!visited[e]){
-                if(dfsUtil(adj , e , visited , pathvisited)){
+                if(toposort(adj , e ,pathvisited, visited )){
                     return true;
                 }
             }
