49b10169178ecb383366585fd84381b355ba3571
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-16 09:35:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,43 +1,41 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
+        ArrayList<ArrayList<Integer>> adj  = new ArrayList<>();
         boolean visited[] = new boolean[V];
-        boolean stack[] = new boolean[V];
-       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-             for(int i = 0; i < V; i++) {
-                 adj.add(new ArrayList<>());
-             }
-             
+        boolean pathvisited[] = new boolean[V];
+        for(int i = 0; i < V; i++){
+            adj.add(new ArrayList<>());
+        }
         for(int e[] : edges){
             int u = e[0];
             int v = e[1];
             adj.get(u).add(v);
         }
-        for(int i = 0; i < adj.size(); i++){
+        for(int i = 0; i < V; i++){
             if(!visited[i]){
-            if( dfsUtilCycleDetection(adj , i , visited , stack)){
-                return true;
+                if(dfsUtil(adj , i , visited , pathvisited)){
+                    return true;
                 }
             }
         }
         return false;
     }
-    boolean dfsUtilCycleDetection( ArrayList<ArrayList<Integer>> adj
-    , int curr , boolean visited[] , boolean stack[]){
+    boolean dfsUtil( ArrayList<ArrayList<Integer>> adj  , int curr ,
+    boolean visited[] , boolean pathvisited[]){
         visited[curr] = true;
-        stack[curr] = true;
+        pathvisited[curr] = true;
         for(int e : adj.get(curr)){
-            if(stack[e]){
+            if(pathvisited[e]){
                 return true;
             }
-           if(!visited[e]){
-                  if(dfsUtilCycleDetection(adj, e, visited, stack)){
-                      return true;
-                  }
-              }
+            if(!visited[e]){
+                if(dfsUtil(adj , e , visited , pathvisited)){
+                    return true;
+                }
+            }
         }
-             stack[curr] =false; 
+        pathvisited[curr] = false;
         return false;
-}
+    }
 }
