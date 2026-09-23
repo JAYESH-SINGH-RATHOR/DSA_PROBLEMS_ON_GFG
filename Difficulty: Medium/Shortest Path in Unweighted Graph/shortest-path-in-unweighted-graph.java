c10e73b1d4da6aa58a0d82827d9c5d6630be4adf
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-24 01:14:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Unweighted Graph/shortest-path-in-unweighted-graph.java
+++ Difficulty: Medium/Shortest Path in Unweighted Graph/shortest-path-in-unweighted-graph.java
@@ -1,52 +1,40 @@
 class Solution {
     public int shortestPath(int V, int[][] edges, int src, int dest) {
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for(int i = 0; i < V; i++) {
+        // code here
+        boolean visited[] = new boolean[V];
+        List<List<Integer>>  adj = new ArrayList<>();
+        for(int i = 0; i < V; i++){
             adj.add(new ArrayList<>());
         }
-
-        for(int e[] : edges) {
+        for(int e[] : edges){
             int u = e[0];
             int v = e[1];
-
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
-        boolean visited[] = new boolean[V];
-
+        int path = 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(src);
-        visited[src] = true;
-
-        int path = 0;
-
-        while(!q.isEmpty()) {
-
-            int size = q.size();
-
-            for(int i = 0; i < size; i++) {
-
+        while(!q.isEmpty()){
+            int s = q.size();
+            for(int i = 0; i < s; i++){
                 int curr = q.remove();
-
-                if(curr == dest) {
+                // q.add(curr);
+                if(curr == dest){
                     return path;
                 }
-
-                for(int e : adj.get(curr)) {
-
-                    if(!visited[e]) {
+                for(int e : adj.get(curr)){
+                    if(!visited[e]){
                         visited[e] = true;
                         q.add(e);
                     }
                 }
             }
-
             path++;
         }
-
         return -1;
     }
 }
+
+
+
